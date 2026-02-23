7c383071df09b865d1dc48893189fa2283575afa
Yasiru Lakshan <yasiru1999@gmail.com>
2026-02-23 22:38:25 +0530

    Enhance navigation handling in bootstrap.js and MarketingApp.js

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,16 +1,24 @@
 import {mount} from 'marketing/MarketingApp';
 import React, {useRef, useEffect} from 'react';
+import {useHistory} from "react-router-dom";
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
+
 
     useEffect(() => {
-        mount(ref.current, {
-            onNavigate: (pathname) => {
-                console.log(" Container is navigating to ", pathname);
+        const {onParentNavigate} = mount(ref.current, {
+            onNavigate: ({pathname: nextPathname}) => {
+                if (history.location.pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+
             }
         });
-    });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref}/>;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -7,9 +7,20 @@ import App from './App';
 const mount = (el, {onNavigate}) => {
     const history = createMemoryHistory();
 
-    history.listen(onNavigate);
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
 
     ReactDOM.render(<App history={history}/>, el);
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const {pathname} = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 // If we are in development and in isolation,
@@ -18,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
